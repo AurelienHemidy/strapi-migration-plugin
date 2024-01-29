401765ff8b1a51fce801b1ca8f5b074498abb32a
AurelienHemidy <aurelien@hemidy.fr>
2024-01-29 22:20:49 +0100

    feat: update main page

--- admin/src/pages/HomePage/index.tsx
+++ admin/src/pages/HomePage/index.tsx
@@ -1,10 +1,4 @@
-/*
- * HomePage
- */
-
-import React, { useEffect, useState } from 'react';
-
-// Strapi
+import React from 'react';
 import {
   Layout,
   BaseHeaderLayout,
@@ -14,91 +8,29 @@ import {
   Flex,
   Button,
 } from '@strapi/design-system';
-import { Refresh } from '@strapi/icons';
-
-// API
-import { migrationRequest } from '../../api/migrations';
-import { migrationConfigRequest } from '../../api/migrations-config';
-import { deprecatedFieldsRequest } from '../../api/deprecated';
+import { Refresh, Play } from '@strapi/icons';
 
 // Components
-import {
-  DeprecatedField,
-  DeprecatedFields,
-} from '../../components/deprecated-fields';
-import {
-  Migration,
-  MigrationsTable,
-} from '../../components/migrations/migrations-table';
-import { MigrationsInfoTile } from '../../components/migrations/migrations-info-tile';
-import { MigrationModeToggle } from '../../components/migrations/migrations-config/migration-mode-toggle';
+import { DeprecatedFields } from '../../components/deprecated-fields/table';
+import { MigrationsTable } from '../../components/migrations/table';
+import { MigrationsInfoTile } from '../../components/migrations/info-tile';
+import { MigrationModeToggle } from '../../components/migrations-config/mode-toggle';
 
-export interface MigrationConfig {
-  id: number;
-  /**
-   * Is migration dry mode active
-   */
-  active_dry_mode: boolean;
-  /**
-   * Is development environment
-   */
-  is_dev: boolean;
-}
+// Hooks
+import { useMigrationsConfig } from '../../hooks/useMigrationsConfig';
+import { useMigrations } from '../../hooks/useMigrations';
 
 const HomePage = () => {
-  const [migrations, setMigrations] = useState<Migration[]>([]);
-  const [migrationsConfig, setMigrationsConfig] = useState<MigrationConfig>();
-  const [deprecatedFields, setDeprecatedFields] = useState<DeprecatedField[]>(
-    []
-  );
-  const [isLoading, setIsLoading] = useState(true);
+  const {
+    migrations,
+    error: migrationsError,
+    fetchMigrations,
+    runMigrations,
+    migrationsSucceeded,
+    migrationsFailed,
+  } = useMigrations();
 
-  const [isDevelopmentEnv, setIsDevelopmentEnv] = useState(false);
-
-  const fetchMigrations = async () => {
-    if (isLoading === false) setIsLoading(true);
-    const migrationsData = await migrationRequest.getAllMigrations();
-
-    setMigrations(migrationsData);
-
-    setIsLoading(false);
-  };
-
-  const fetchMigrationsConfig = async () => {
-    if (isLoading === false) setIsLoading(true);
-    const migrationsConfigData = await migrationConfigRequest.getConfig();
-
-    setMigrationsConfig(migrationsConfigData);
-    setIsDevelopmentEnv(migrationsConfigData.is_dev);
-
-    setIsLoading(false);
-  };
-
-  const fetchDeprecatedFields = async () => {
-    const deprecatedFieldsData: DeprecatedField[] =
-      await deprecatedFieldsRequest.getAllDeprecatedFields();
-
-    setDeprecatedFields(deprecatedFieldsData);
-  };
-
-  const toggleDryMode = async () => {
-    await migrationConfigRequest.toggleDryMode();
-
-    await fetchMigrationsConfig();
-  };
-
-  useEffect(() => {
-    fetchMigrations();
-    fetchMigrationsConfig();
-    fetchDeprecatedFields();
-  }, []);
-
-  const migrationsSucceeded = migrations.filter(
-    migration => migration.status === 'success'
-  );
-  const migrationsFailed = migrations.filter(
-    migration => migration.status === 'fail'
-  );
+  const { isDevelopmentEnv } = useMigrationsConfig();
 
   return (
     <Layout>
@@ -107,59 +39,60 @@ const HomePage = () => {
         subtitle="All your migrations in one place."
         as="h2"
       />
-
       <ContentLayout>
         <Box paddingBottom={4}>
-          <MigrationModeToggle
-            title="Dry mode"
-            isDevelopmentEnv={isDevelopmentEnv}
-            activeDryMode={migrationsConfig?.active_dry_mode ?? true}
-            toggleDryMode={toggleDryMode}
-          />
+          <MigrationModeToggle title="Dry mode" />
           <Box paddingTop={7}>
             <Flex gap={2} justifyContent="space-between">
-              <Flex gap={2}>
-                <MigrationsInfoTile
-                  text="migrations run"
-                  number={migrations.length}
-                  textColor="neutral800"
-                  isMigrationsArrayEmpty={!migrations.length}
-                />
-                <MigrationsInfoTile
-                  text="migrations succeeded"
-                  number={migrationsSucceeded.length}
-                  textColor="success500"
-                  isMigrationsArrayEmpty={!migrationsSucceeded.length}
-                />
-                <MigrationsInfoTile
-                  text="migrations failed"
-                  number={migrationsFailed.length}
-                  textColor="danger500"
-                  isMigrationsArrayEmpty={!migrationsFailed.length}
-                />
-              </Flex>
-              {isDevelopmentEnv && (
-                <Button variant="secondary" onClick={fetchMigrations}>
-                  <Flex>
-                    <Icon as={Refresh} marginRight={2} />
-                    Refresh
+              {!migrationsError && (
+                <>
+                  <Flex gap={2}>
+                    <MigrationsInfoTile
+                      text="migrations run"
+                      number={migrations.length}
+                      textColor="neutral800"
+                      isMigrationsArrayEmpty={!migrations.length}
+                    />
+                    <MigrationsInfoTile
+                      text="migrations succeeded"
+                      number={migrationsSucceeded.length}
+                      textColor="success500"
+                      isMigrationsArrayEmpty={!migrationsSucceeded.length}
+                    />
+                    <MigrationsInfoTile
+                      text="migrations failed"
+                      number={migrationsFailed.length}
+                      textColor="danger500"
+                      isMigrationsArrayEmpty={!migrationsFailed.length}
+                    />
                   </Flex>
-                </Button>
+                  <Flex gap={2}>
+                    {isDevelopmentEnv && (
+                      <Button onClick={runMigrations}>
+                        <Flex>
+                          <Icon as={Play} marginRight={2} />
+                          Run migrations
+                        </Flex>
+                      </Button>
+                    )}
+                    {isDevelopmentEnv && (
+                      <Button variant="secondary" onClick={fetchMigrations}>
+                        <Flex>
+                          <Icon as={Refresh} marginRight={2} />
+                          Refresh
+                        </Flex>
+                      </Button>
+                    )}
+                  </Flex>
+                </>
               )}
             </Flex>
           </Box>
         </Box>
 
-        <MigrationsTable
-          migrations={migrations}
-          isMigrationsLoading={isLoading}
-        />
+        <MigrationsTable />
 
-        <DeprecatedFields
-          deprecatedFields={deprecatedFields}
-          fetchDeprecatedFields={fetchDeprecatedFields}
-          isDevelopmentEnv={isDevelopmentEnv}
-        />
+        <DeprecatedFields />
       </ContentLayout>
     </Layout>
   );
