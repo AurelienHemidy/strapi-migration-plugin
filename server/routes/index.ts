a189721996d85e9a1d70811b9c0955d95a759d34
AurelienHemidy <aurelien@hemidy.fr>
2024-01-29 22:23:48 +0100

    feat: update server imports

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,38 +1,11 @@
-export default [
-  {
-    method: 'GET',
-    path: '/find',
-    handler: 'migrations.find',
-    config: {
-      policies: [],
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/config',
-    handler: 'migrationsConfig.find',
-    config: {
-      policies: [],
-      auth: false,
-    },
-  },
-  {
-    method: 'PATCH',
-    path: '/config/toggle-dry-mode',
-    handler: 'migrationsConfig.toggleDryMode',
-    config: {
-      policies: [],
-      auth: false,
-    },
-  },
-  {
-    method: 'GET',
-    path: '/deprecated-fields/find',
-    handler: 'deprecatedFields.find',
-    config: {
-      policies: [],
-      auth: false,
-    },
-  },
-];
+import migrations from './migrations';
+import migrationsConfig from './migrations-config';
+import deprecatedFields from './deprecated-fields';
+import contentTypes from './content-types';
+
+export default {
+  migrations,
+  migrationsConfig,
+  deprecatedFields,
+  contentTypes,
+};
--- server/services/index.ts
+++ server/services/index.ts
@@ -1,9 +1,11 @@
 import migrations from './migrations';
 import migrationsConfig from './migrations-config';
 import deprecatedFields from './deprecated-fields';
+import contentTypes from './content-types';
 
 export default {
   migrations,
   migrationsConfig,
   deprecatedFields,
+  contentTypes,
 };
